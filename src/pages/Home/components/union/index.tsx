aa8254c2f59bda3532c935e88bea417ee541516c
Mr-lai <xinruoxiangzhilove@163.com>
2024-03-27 14:50:07 +0800

    style: 完善复式选号样式

--- src/pages/Home/components/union/index.tsx
+++ src/pages/Home/components/union/index.tsx
@@ -1,8 +1,10 @@
-import { Stepper,Button,Radio,Space } from 'antd-mobile';
+import { Stepper,Button,Radio,Space,List } from 'antd-mobile';
 import { useState } from 'react'
 import classNames from 'classnames/bind'
 import style from './index.module.scss';
 import KillNumber from '../killNumber/index';
+import redCircle from '@/assets/redCircle.png';
+import blueCircle from '@/assets/blueCircle.png';
 import { handleConventionArr ,handleDoubleArr} from '@/hook/index';
 const cla = classNames.bind(style)
 const Union = (props) => {
@@ -12,7 +14,8 @@ const Union = (props) => {
   const [stepperValue, setStepperValue] = useState(1)
   const [radioValue, setRadioValue] = useState('rule')
   const [redDouble,setRedDouble] = useState(6)
-  const [blueDouble,setblueDouble] = useState(1)
+  const [blueDouble, setblueDouble] = useState(1)
+  const [lotteryArr, setLotteryArr] = useState([])
   const [killBlueObj, setKillBlueObj] = useState(
     {
       value: '蓝区杀号',
@@ -28,15 +31,13 @@ const Union = (props) => {
     }
   )
 
-  const automaticSelection = (type) => {
+  const automaticSelection =async (type) => {
     if (type === 'rule') {
-      const ruleArr = handleConventionArr(killBlueObj.arr, killRedObj.arr, props.value, radioValue, stepperValue)
-      props.setLotteryArr(ruleArr)
+      const ruleArr = await handleConventionArr(killBlueObj.arr, killRedObj.arr, props.value, radioValue, stepperValue)
+     setLotteryArr(ruleArr)
     } else { 
-      const doubleArr = handleDoubleArr(killBlueObj.arr, killRedObj.arr, props.value, radioValue, redDouble,blueDouble) 
-      console.log(doubleArr);
-      // props.setLotteryDoubleArr(doubleArr)
-      props.setLotteryArr(doubleArr)
+      const doubleArr = await handleDoubleArr(killBlueObj.arr, killRedObj.arr, props.value, radioValue, redDouble,blueDouble) 
+      setLotteryArr(doubleArr)
     }
   }
 
@@ -46,7 +47,7 @@ const Union = (props) => {
         setRadioValue(`${value}`)
         setInpValue('')
         setInpValue1('')
-        props.setLotteryArr([])
+        setLotteryArr([])
         setStepperValue(1)
       }}>
           <Space direction='vertical'>
@@ -57,30 +58,66 @@ const Union = (props) => {
       <KillNumber killObj={killRedObj} inpValue={inpValue} setInpValue={ setInpValue} setKillObj={ setKillRedObj } />
       <KillNumber killObj={killBlueObj} inpValue={inpValue1} setInpValue={ setInpValue1} setKillObj={ setKillBlueObj } />
     {
-       radioValue==='rule' && <div className='machine-selection'>
-            <span>机选常规</span>
-              <Stepper
-              defaultValue={1}
-            min={0}
-            value={stepperValue }
-              onChange={value => {
-                setStepperValue(value)
-              }}
-              />
-          <span className='machine-unit'>注</span>
-              <Button color='danger' size='mini' fill='solid'
-                onClick={() => {
-                  automaticSelection('rule')
-                }}
-              >
-              开始机选
-          </Button>
-          <Button color='warning' size='mini' fill='solid'
-            onClick={() => {
-                props.setLotteryArr([])
-                setStepperValue(1)
-                          }}>清除</Button>
+        radioValue === 'rule' && <div>
+
+              <div className='machine-selection'>
+                    <span>机选常规</span>
+                      <Stepper
+                        defaultValue={1}
+                        min={0}
+                        value={stepperValue }
+                        onChange={value => {
+                          setStepperValue(value)
+                        }}
+                      />
+                      <span className='machine-unit'>注</span>
+                      <Button color='danger' size='mini' fill='solid'
+                        onClick={() => {
+                          automaticSelection('rule')
+                        }}
+                      >
+                          开始机选
+                    </Button>
+                    <Button color='warning' size='mini' fill='solid'
+                        onClick={() => {
+                            setLotteryArr([])
+                            setStepperValue(1)
+                      }}>清除</Button>
+              </div>
+              <div>
+                {lotteryArr.length>0 && lotteryArr.map((item,index) => { 
+                      return ( 
+                        <List.Item key={`${index}index`}>
+                          <div className='lottery-index'>{index+1 }.</div>
+                          <ul className="red-item">
+                              {item.red.length>0 && item.red.map((items,indexs) => {
+                                return (
+                                  <li className='lotteryNumContainer redLottery' key={indexs+'index'}>
+                                  <img src={redCircle} alt="" />
+                                    <div className="lotteryNum">{ items}</div>
+                                  </li>
+                                )
+                              }) }
+                  
+                          </ul>
+                        <ul className="blu-item">
+                            { item.blue.length > 0 && item.blue.map((items, indexs) => {
+                                return (
+                                  <li className="lotteryNumContainer" key={`${indexs}${items}`}>
+                                  <img src={ blueCircle} alt="" />
+                                    <div className="lotteryNum lotteryNumBlue">{ items}</div>
+                              </li>
+                                )
+                              })
+                          }
+                        </ul>
+                      </List.Item>
+                      )
+                })}
+              </div>
         </div>
+        
+        
     }
     {
        radioValue==='double'  && <div >
@@ -110,21 +147,58 @@ const Union = (props) => {
             />
             <span className='machine-unit'>位</span>
           </div>
-          
-          <Button color='danger' size='small' fill='solid'
-                          onClick={() => {
-                            automaticSelection('double')
-                          }}
-                        >
-                        开始机选
-            </Button>
+            <div className={ cla('double-btn') }>
+                 
+            <Button color='danger' size='small' fill='solid'
+                            onClick={() => {
+                              automaticSelection('double')
+                            }}
+                          >
+                          开始机选
+              </Button>
             <Button color='warning' size='small' fill='solid'    onClick={() => {
-                           props.setLotteryArr([])
+                           setLotteryArr([])
                            setStepperValue(1)
                           }}>清除</Button>
-        </div>
+          </div>
+          </div>
+          <div className={ cla('double-main')}>
+            {lotteryArr.length>0 && lotteryArr.map((item,index) => { 
+                  return ( 
+                    <List.Item key={`${index}index`}>
+                      {/* <div className='lottery-index'>{index+1 }.</div> */}
+                      <div>
+                        <div className={ cla('title-text')}>红区:</div>
+                        <ul className={cla('double-red-item')}>
+                          {item.red.length>0 && item.red.map((items,indexs) => {
+                            return (
+                              <li className={cla('lotteryNumContainer')} key={indexs+'index'}>
+                              <img src={redCircle} alt="" />
+                                <div className={ cla("lotteryNum")}>{ items}</div>
+                              </li>
+                            )
+                          }) }
+              
+                      </ul>
+                        <div className={ cla('title-text')}>篮区:</div>
+                        <ul className={cla('double-blue-item')}>
+                        { item.blue.length > 0 && item.blue.map((items, indexs) => {
+                            return (
+                              <li className={cla('lotteryNumContainer')} key={`${indexs}${items}`}>
+                              <img src={ blueCircle} alt="" />
+                                <div className={cla("lotteryNum") }>{ items}</div>
+                          </li>
+                            )
+                          })
+                      }
+                    </ul>
+                     </div>
+                  </List.Item>
+                  )
+                })}
+          </div>
       </div>
-    }
+      }
 </div>
   )
 }
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -4,14 +4,10 @@ import '@/style/index.scss';
 import title from '@/assets/title.png'
 import Union from './components/union';
 import { useState } from 'react'
-import redCircle from '@/assets/redCircle.png';
-import blueCircle from '@/assets/blueCircle.png';
 import store from '@/store';
 console.log(store);
 const Home = () => {
   const [tabsValue, setTabsValue] = useState('union')
-  const [lotteryArr, setLotteryArr] = useState([])
-  const [lotteryDoubleArr, setLotteryDoubleArr] = useState([])
   return (
     <div>
       <div className="home-title">
@@ -23,60 +19,27 @@ const Home = () => {
         }}>
           <Tabs.Tab title='双色球' key='union'>
           双色球自助选号
-          <Union value={tabsValue} setLotteryArr={setLotteryArr} setLotteryDoubleArr={ setLotteryDoubleArr} />
-          {lotteryArr.length>0 && lotteryArr.map((item,index) => { 
-            return ( 
-              <List.Item key={`${index}index`}>
-                <div className='lottery-index'>{index+1 }.</div>
-                <ul className="red-item">
-                    {item.red.length>0 && item.red.map((items,indexs) => {
-                      return (
-                        <li className='lotteryNumContainer redLottery' key={indexs+'index'}>
-                        <img src={redCircle} alt="" />
-                          <div className="lotteryNum">{ items}</div>
-                        </li>
-                      )
-                    }) }
-        
-                </ul>
-              <ul className="blu-item">
-                  { item.blue.length > 0 && item.blue.map((items, indexs) => {
-                      return (
-                        <li className="lotteryNumContainer" key={`${indexs}${items}`}>
-                        <img src={ blueCircle} alt="" />
-                          <div className="lotteryNum lotteryNumBlue">{ items}</div>
-                    </li>
-                      )
-                    })
-                }
-              </ul>
-            </List.Item>
-            )
-          })}
+          <Union value={tabsValue} />
           </Tabs.Tab>
           <Tabs.Tab title='大乐透' key='lotto' >
           大乐透
-          <List.Item>1</List.Item>
-          <List.Item>2</List.Item>
-          <List.Item>3</List.Item>
+          <List.Item>敬请期待</List.Item>
+
           </Tabs.Tab>
           <Tabs.Tab title='快乐8' key='eight'>
           快乐8
-          <List.Item>1</List.Item>
-          <List.Item>2</List.Item>
-          <List.Item>3</List.Item>
+          <List.Item>敬请期待</List.Item>
+
           </Tabs.Tab>
           <Tabs.Tab title='七乐彩' key='seven'>
           七乐彩
-          <List.Item>1</List.Item>
-          <List.Item>2</List.Item>
-          <List.Item>3</List.Item>
+          <List.Item>敬请期待</List.Item>
+
           </Tabs.Tab>
           <Tabs.Tab title='3D' key='three'>
           3D
-          <List.Item>1</List.Item>
-          <List.Item>2</List.Item>
-          <List.Item>3</List.Item>
+          <List.Item>敬请期待</List.Item>
+
           </Tabs.Tab>
         </Tabs>
       </div>
